92b71fbaa194db6726a2598de76f162579103393
iKizuna <piotrjazwinski94@gazeta.pl>
2025-05-30 19:33:17 +0200

    final project

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from stats import words, get_book_text
+import sys
+from stats import words, get_book_text, character_appears, pretty_dictionary
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
-    number_of_words = words(book_text)
-    print(f"{number_of_words} words found in the document")
-
+    if len(sys.argv) == 2:
+        file_path = sys.argv[1]
+        book_text = get_book_text(file_path)
+        number_of_words = words(book_text)
+        number_of_characters = character_appears(book_text)
+        pretty_words = pretty_dictionary(number_of_characters)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {number_of_words} total words")
+        print("--------- Character Count -------")
+        for entry in pretty_words:
+            print(f"{entry['char']}: {entry['num']}")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,21 @@ def get_book_text(filepath):
 def words(text):
     words = text.split()
     return len(words)
+
+def character_appears(book_text):
+    lower_text = book_text.lower()
+    character_appears = {}
+    for char in lower_text:
+        if char.isalpha():
+            character_appears[char] = character_appears.get(char, 0) + 1  
+    return character_appears
+
+def sort_on(dict):
+    return dict["num"]
+
+def pretty_dictionary(dictionary):
+    # Create a list of {'char': ..., 'num': ...} dictionaries
+    character_list = [{'char': char, "num": count} for char, count in dictionary.items()]
+    # Sorts the character_list
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
